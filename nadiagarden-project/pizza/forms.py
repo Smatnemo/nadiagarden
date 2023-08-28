b4b9883088eab09f7ed2a7b2375c22f272e00da7
Samuel Tertese Moses <samuel@2e-volve.local>
2023-08-28 17:01:48 +0100

    forms

---
+++ nadiagarden-project/pizza/forms.py
@@ -0,0 +1,20 @@
+from django import forms 
+from .models import Pizza, Size
+
+# class PizzaForm(forms.Form):
+#     topping1 = forms.CharField(label='Topping 1', max_length=100)
+#     topping2 = forms.CharField(label='Topping 2', max_length=100)
+#     # toppings = forms.MultipleChoiceField(choices=[('pep','Pepperoni'),('cheese','Cheese'),('olives','Olives')], widget=forms.CheckboxSelectMultiple)
+    # size = forms.ChoiceField(label='Size', choices=[('Small', 'Small'),('Medium', 'Medium'), ('Large', 'Large')])
+
+# This form takes the arguments of fields from the forms.py
+class PizzaForm(forms.ModelForm):
+
+    # size = forms.ModelChoiceField(queryset=Size.objects, empty_label=None, widget=forms.RadioSelect)
+    image = forms.ImageField()
+
+    class Meta:
+        model = Pizza 
+        fields = ['topping1', 'topping2', 'size']
+        labels = {'topping1': 'Topping 1', 'topping2': 'Topping 2'}
+        
